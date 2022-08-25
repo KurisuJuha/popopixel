dc5fb237fc618d4a782f0ca037adb7cf0b0fbf81
KYapp <kukiyomenaisapp@gmail.com>
2022-08-26 00:20:18 +0900

    Init

---
+++ popopixel/Game.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace popopixel
+{
+    public class Game : Microsoft.Xna.Framework.Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private Texture2D texture2D;
+
+        public Game()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            texture2D = new Texture2D(GraphicsDevice, 128, 128);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            texture2D = new Texture2D(GraphicsDevice, 128, 128);
+            Color[] data = new Color[128 * 128];
+            for (int pixel = 0; pixel < data.Length; pixel++)
+            {
+                data[pixel] = Color.Black;
+            }
+            texture2D.SetData(data);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.AliceBlue);
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(texture2D, new Vector2(0, 0), Color.White);
+
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ popopixel/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new popopixel.Game();
+game.Run();
